c0c05f9ae71f2cca55920dbceb2c1660c9751ffd
yong <yong.yongyao@gmail.com>
2020-06-17 15:05:14 -0400

    add test program

---
+++ chewietest/lib/main.dart
@@ -0,0 +1,198 @@
+import 'package:chewie/chewie.dart';
+import 'package:chewie/src/chewie_player.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(
+    ChewieDemo(),
+  );
+}
+
+class ChewieDemo extends StatefulWidget {
+  ChewieDemo({this.title = 'Hls stream player'});
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ChewieDemoState();
+  }
+}
+
+class _ChewieDemoState extends State<ChewieDemo> {
+  TargetPlatform _platform;
+  VideoPlayerController _videoPlayerController1;
+  VideoPlayerController _videoPlayerController2;
+  ChewieController _chewieController;
+  String timeCode = "";
+  String closedcaption = "";
+  bool ccInited = false;
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController1 = VideoPlayerController.network(
+        'https://parlvulive-dt.azureedge.net/HOC230-X-1/Test/VL/EN/Playlist.m3u8');
+    _videoPlayerController1.addListener(_updateState);
+
+    _videoPlayerController2 = VideoPlayerController.network(
+        'https://parlvu-vod.parl.gc.ca/pvvodhoc-en/_definst_/mp4:azrhoc/archives/PV750/2019/2019-04-02/31241_FAAE%20Meeting%20No.%20133_08-46-57_VL1.mp4/playlist.m3u8?audioindex=1&wowzalanguage=eng');
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController1,
+      aspectRatio: 3 / 2,
+      autoPlay: true,
+      looping: true,
+      showSubtitle :true
+      // Try playing around with some of these other options:
+
+      // showControls: false,
+      // materialProgressColors: ChewieProgressColors(
+      //   playedColor: Colors.red,
+      //   handleColor: Colors.blue,
+      //   backgroundColor: Colors.grey,
+      //   bufferedColor: Colors.lightGreen,
+      // ),
+      // placeholder: Container(
+      //   color: Colors.grey,
+      // ),
+      // autoInitialize: true,
+    );
+  }
+
+
+
+  @override
+  void dispose() {
+    _videoPlayerController1.removeListener(_updateState);
+    _videoPlayerController1.dispose();
+    _videoPlayerController2.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: widget.title,
+      theme: ThemeData.light().copyWith(
+        platform: _platform ?? Theme.of(context).platform,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Chewie(
+                  controller: _chewieController,
+                ),
+              ),
+            ),
+            Text(timeCode),
+            Text(closedcaption),
+            FlatButton(
+              onPressed: () {
+                _chewieController.enterFullScreen();
+              },
+              child: Text('Fullscreen'),
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _chewieController.dispose();
+                        _videoPlayerController2.pause();
+                        //_videoPlayerController2.seekTo(Duration(seconds: 0));
+                        _chewieController = ChewieController(
+                          videoPlayerController: _videoPlayerController1,
+                          aspectRatio: 3 / 2,
+                          autoPlay: true,
+                          looping: true,
+                          showSubtitle: true
+                        );
+                      });
+                    },
+                    child: Padding(
+                      child: Text("Live stream"),
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _chewieController.dispose();
+                        _videoPlayerController1.pause();
+                        //_videoPlayerController1.seekTo(Duration(seconds: 0));
+                        _chewieController = ChewieController(
+                          videoPlayerController: _videoPlayerController2,
+                          aspectRatio: 3 / 2,
+                          autoPlay: true,
+                          looping: true,
+                        );
+                      });
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text("Archive Video"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _platform = TargetPlatform.android;
+                      });
+                    },
+                    child: Padding(
+                      child: Text("Android controls"),
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _platform = TargetPlatform.iOS;
+                      });
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text("iOS controls"),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  
+  void _updateState() {
+    setState(() {
+      timeCode = _videoPlayerController1.value.metadata;
+      closedcaption = _videoPlayerController1.value.subtitle;
+      if (_videoPlayerController1.value.subtitleList.isEmpty == false && _videoPlayerController1.value.subtitleList.length > 0 && ccInited == false) {
+        ccInited = true;
+        _videoPlayerController1.setSubtitles(
+            _videoPlayerController1.value.subtitleList[0].trackIndex,
+            _videoPlayerController1.value.subtitleList[0].groupIndex
+        );
+      }
+    });
+  }
+}
